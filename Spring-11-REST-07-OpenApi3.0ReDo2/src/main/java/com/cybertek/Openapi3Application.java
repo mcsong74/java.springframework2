8344206ae31e9cdb539194da0f6818285e02007e
ssonata74 <ssonata@gmail.com>
2021-02-05 22:19:32 -0500

    Springframework continue - Open API 3.0 redo

--- Spring-11-REST-07-OpenApi3.0ReDo2/src/main/java/com/cybertek/Openapi3Application.java
+++ Spring-11-REST-07-OpenApi3.0ReDo2/src/main/java/com/cybertek/Openapi3Application.java
@@ -1,7 +1,10 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Openapi3Application {
@@ -10,4 +13,11 @@ public class Openapi3Application {
 		SpringApplication.run(Openapi3Application.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenApi(){
+		return new OpenAPI()
+				.info(new Info().title("Cinema Application")
+						.version("v1")
+						.description("Selling online cinema tickets"));
+	}
 }
