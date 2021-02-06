28efe5c78fbba38c0b2593ed3be432ce896a4e01
ssonata74 <ssonata@gmail.com>
2021-02-05 22:04:18 -0500

    Springframework continue - Open API 3.0 redo

--- Spring-11-REST-07-OpenApi3.0ReDo2/src/main/java/com/cybertek/controller/UserController.java
+++ Spring-11-REST-07-OpenApi3.0ReDo2/src/main/java/com/cybertek/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name="User", description = "User API - CRUD operation")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
