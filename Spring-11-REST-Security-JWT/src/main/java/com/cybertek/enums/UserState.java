ba47885cd8045830ba939a194cf54a9109640b6c
ssonata74 <ssonata@gmail.com>
2021-02-06 11:38:12 -0500

    Spring Security - JWT

---
+++ Spring-11-REST-Security-JWT/src/main/java/com/cybertek/enums/UserState.java
@@ -0,0 +1,5 @@
+package com.cybertek.enums;
+
+public enum UserState {
+    PENDING, ACTIVE, SUSPENDED, NO_LONGER_EMPLOYEE;
+}
---
+++ Spring-11-REST-Security-JWT/src/main/java/com/cybertek/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    //for authentication
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+
+}
