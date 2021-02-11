59ea524ad4d13e18b42e6a50db00596af1cec0a4
ssonata74 <ssonata@gmail.com>
2021-02-10 19:09:12 -0500

    Spring Security - JWT - Exception handling

--- Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
+++ Spring-11-REST-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
@@ -8,20 +8,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+
 @RestControllerAdvice //global
 @Order(Ordered.HIGHEST_PRECEDENCE) // give priority to spring framework prior to Spring's exception
 public class ExceptionMessageHandler {
+    //building custom exception
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ResponseWrapper> serviceException(ServiceException se){
         String message = se.getMessage();
 
-        return new ResponseEntity<>(ResponseWrapper
-                .builder()
+        return new ResponseEntity<>(ResponseWrapper.builder()
                 .success(false)
                 .code(HttpStatus.CONFLICT.value())
                 .message(message)
                 .build(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseWrapper> accessDeniedException(AccessDeniedException ae){
+        String message = ae.getMessage();
+
+        return new ResponseEntity<>(ResponseWrapper.builder()
+                .success(false)
+                .code(HttpStatus.FORBIDDEN.value())
+                .message(message)
+                .build(), HttpStatus.FORBIDDEN);
     }
 
 
