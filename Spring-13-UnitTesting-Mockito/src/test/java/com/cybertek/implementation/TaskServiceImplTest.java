f2854fa077546c358a6c45a2a564ea5d07a4bb3d
ssonata74 <ssonata@gmail.com>
2021-03-02 21:22:12 -0500

    Spring - Unit testing - Mockito

--- Spring-13-UnitTesting-Mockito/src/test/java/com/cybertek/implementation/TaskServiceImplTest.java
+++ Spring-13-UnitTesting-Mockito/src/test/java/com/cybertek/implementation/TaskServiceImplTest.java
@@ -16,8 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
@@ -45,6 +46,20 @@ class TaskServiceImplTest {
 
         assertNotNull(taskDTO1);
 
+    }
+    @Test
+    void findByIdBddTest(){
+        //given
+        Task task = new Task();
+        given(taskRepository.findById(Mockito.anyLong())).willReturn(Optional.of(task));
+        given(taskMapper.convertToDto(task)).willReturn(new TaskDTO());
+        //when
+        TaskDTO taskDTO = taskService.findById(Mockito.anyLong());
+        //then
+        then(taskRepository).should().findById(Mockito.anyLong());
+        then(taskRepository).should(never()).findById(-5L);
+
+        assertNotNull(taskDTO);
     }
 
 }
