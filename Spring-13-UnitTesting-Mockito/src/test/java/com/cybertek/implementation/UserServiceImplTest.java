ea8df608789df4c09ff1c705062a8bc8f78aaa92
ssonata74 <ssonata@gmail.com>
2021-03-02 19:32:19 -0500

    Spring - Unit testing - Mockito

---
+++ Spring-13-UnitTesting-Mockito/src/test/java/com/cybertek/implementation/UserServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.cybertek.implementation;
+
+import com.cybertek.mapper.MapperUtil;
+import com.cybertek.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class) // to use Mockito, this annotation is required
+class UserServiceImplTest {
+
+    @Mock
+    UserRepository userRepository; //deleteByUserName using UserRepository, need to mock the repository
+    @Mock
+    MapperUtil mapperUtil;
+
+    @InjectMocks //above Mocks will be injected below UserServiceImpl
+    UserServiceImpl userService; //has dependency to UserRepository(in UserServiceImplementation, UserRepository is
+    // injected and which created dependency, so need to add @InjectMocks annotation.
+
+    @Test
+    void deleteByUserName() {
+        //call method from service implementation for testing
+        userService.deleteByUserName("mike@cybertek.com");
+        //verification
+//        verify(userRepository).deleteByUserName("mike@cybertek.com");
+//        verify(userRepository, times(2)).deleteByUserName("mike@cybertek.com");
+//        verify(userRepository, atLeastOnce()).deleteByUserName("mike@cybertek.com");
+        verify(userRepository, atMost(5)).deleteByUserName("mike@cybertek.com");
+
+
+    }
+}
