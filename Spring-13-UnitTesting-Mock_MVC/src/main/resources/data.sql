6fc1a425043db742d10ff0ccdef0c538d4b9a458
ssonata74 <ssonata@gmail.com>
2021-03-06 11:56:02 -0500

    Spring - Unit testing - Mock MVC

---
+++ Spring-13-UnitTesting-Mock_MVC/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into student(first_name, last_name, age) values('Mike', 'Smith', 25);
+insert into student(first_name, last_name, age) values('Ozzy', 'Cyber', 30);
+insert into student(first_name, last_name, age) values('Jessica', 'Alba', 26);
+
+
+
+
