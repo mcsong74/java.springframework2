c0e6bd4a800907359530efdfe8c32fb23f5f6e12
ssonata74 <ssonata@gmail.com>
2021-03-06 11:38:25 -0500

    Spring - Unit testing - Mock MVC

--- Spring-13-UnitTesting-Mock_MVC/src/test/java/com/cybertek/controller/WelcomeControllerTest.java
+++ Spring-13-UnitTesting-Mock_MVC/src/test/java/com/cybertek/controller/WelcomeControllerTest.java
@@ -11,11 +11,12 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(WelcomeController.class) //only test executes only in WelcomeController
 //@SpringBootTest  //either one annotation
 class WelcomeControllerTest {
-
+//Controller level testing
     @Autowired
     private MockMvc mockMvc; //for Mock API
 
@@ -28,13 +29,18 @@ class WelcomeControllerTest {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/welcome").accept(MediaType.APPLICATION_JSON);
         //2. perform mock end point and return the result
         MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
-        //and verify "welcome" returned
+        //3. and verify "welcome" returned
         assertEquals("welcome", mvcResult.getResponse().getContentAsString());
     }
 
     @Test
-    void welcome2(){
+    void welcome2() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/welcome").accept(MediaType.APPLICATION_JSON);
+        //same as 2-3 above test
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("welcome"))
+                .andReturn();
 
 
 
