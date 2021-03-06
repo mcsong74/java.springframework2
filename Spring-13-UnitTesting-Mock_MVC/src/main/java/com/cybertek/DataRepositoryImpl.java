be4ebddfa0462d6ecd76703c7e56d517422833ef
ssonata74 <ssonata@gmail.com>
2021-03-06 10:33:17 -0500

    Spring - Unit testing - Mock MVC

---
+++ Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/DataRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.cybertek;
+
+public class DataRepositoryImpl implements DataRepository{
+
+    @Override
+    public int[] findAll() {
+        return new int[]{1,2,3};
+    }
+}
--- Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/UnitTestingImpl.java
+++ Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/UnitTestingImpl.java
@@ -3,6 +3,8 @@ package com.cybertek;
 import java.util.Arrays;
 
 public class UnitTestingImpl {
+
+
     public int calculateSum(int[] data){
         int sum = 0;
         return Arrays.stream(data).sum();
