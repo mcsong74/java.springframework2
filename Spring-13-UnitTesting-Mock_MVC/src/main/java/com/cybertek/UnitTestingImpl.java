c73e0d83be0cd9b37d2f866aa4c5dc6b9fdb1e30
ssonata74 <ssonata@gmail.com>
2021-03-06 10:29:28 -0500

    Spring - Unit testing - Mock MVC

---
+++ Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/UnitTestingImpl.java
@@ -0,0 +1,10 @@
+package com.cybertek;
+
+import java.util.Arrays;
+
+public class UnitTestingImpl {
+    public int calculateSum(int[] data){
+        int sum = 0;
+        return Arrays.stream(data).sum();
+    }
+}
