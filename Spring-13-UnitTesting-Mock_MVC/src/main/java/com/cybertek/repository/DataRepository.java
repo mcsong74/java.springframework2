0554ede8eb459fbfc5cecd501a9dcff8cafaafb9
ssonata74 <ssonata@gmail.com>
2021-03-06 10:48:49 -0500

    Spring - Unit testing - Mock MVC

--- Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/repository/DataRepository.java
+++ Spring-13-UnitTesting-Mock_MVC/src/main/java/com/cybertek/repository/DataRepository.java
@@ -3,7 +3,7 @@ package com.cybertek.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface DataRepository {
     int[] findAll();
 }
--- Spring-13-UnitTesting-Mock_MVC/src/test/java/com/cybertek/UnitTestingImplTest.java
+++ Spring-13-UnitTesting-Mock_MVC/src/test/java/com/cybertek/UnitTestingImplTest.java
@@ -3,9 +3,15 @@ package com.cybertek;
 import com.cybertek.repository.DataRepository;
 import com.cybertek.repository.DataRepositoryImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UnitTestingImplTest {
 
     @Test
@@ -17,9 +23,25 @@ class UnitTestingImplTest {
 
     @Test
     void calculateSumUsingDataService(){
+        // this has data layer + method  testing, Unit testing is simplest say possible, so need to sepearte layer
         DataRepositoryImpl dataRepository = new DataRepositoryImpl();
         UnitTestingImpl unitTesting = new UnitTestingImpl(dataRepository);
         int actual = unitTesting.calculateSumUsingDtaService();
-        assertEquals(5, actual);
+        assertEquals(6, actual);
     }
+
+    @Mock
+    DataRepository dataRepository;
+    @InjectMocks
+    UnitTestingImpl unitTesting;
+
+    @Test
+    void calculateSumUsingDataServiceUsingMock(){
+        //separated data layer using Mock to test unitTesting.calculateSumUsingDtaService()
+        int sum = 0;
+        when(dataRepository.findAll()).thenReturn(new int[] {1,2,3});
+        int actual = unitTesting.calculateSumUsingDtaService(); //goal for unit testing here
+        assertEquals(6, actual);
+    }
+
 }
